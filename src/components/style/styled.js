43bcda38f2fc1d1cb45e61e138ed6170580351d5
phalves23 <rm93693@fiap.com.br>
2022-09-30 23:37:11 -0300

    :lipstick: estilizando DivFichas

---
+++ src/components/style/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const DivFichas = styled.div`
+  gap: 2rem;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 1440px;
+`;
