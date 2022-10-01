70a26297c962abe33d10e8183cf63f4a986fdfad
phalves23 <rm93693@fiap.com.br>
2022-09-30 23:37:38 -0300

    :lipstick: estilizando DivFicha

--- src/components/style/styled.js
+++ src/components/style/styled.js
@@ -8,3 +8,30 @@ export const DivFichas = styled.div`
   flex-wrap: wrap;
   max-width: 1440px;
 `;
+export const DivFicha = styled.div`
+  border-radius: 10px;
+  /* margin: 0 auto; */
+  background-color: #f2f2f2;
+  padding: 1rem;
+  width: 300px;
+  height: 600px;
+
+  h2 {
+    text-align: center;
+  }
+  img {
+    width: 100%;
+    border-radius: 10px;
+    margin-bottom: 0.5rem;
+    height: 200px;
+  }
+  p {
+    margin-bottom: 1rem;
+  }
+  span {
+    font-weight: 600;
+    &.obs {
+      display: block;
+    }
+  }
+`;
