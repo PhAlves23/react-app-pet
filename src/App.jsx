12cf96350462676e6fc4ad132358c6f2881d37f9
phalves23 <rm93693@fiap.com.br>
2022-09-30 23:55:12 -0300

    :sparkles: Criando componente App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Ficha from "./components/ficha/Ficha";
+import PetCadastro from "./components/petCadastro/PetCadastro";
+
+export default function App() {
+  return (
+    <>
+      <PetCadastro />
+    </>
+  );
+}
